a38118c9e4992c1d971564f6d360d222a0b35aac
jieun <jkdev1324@gmail.com>
2025-06-14 20:35:33 +0900

    DP - Memoization

--- src/leetcode/CountSortedVowelStrings.java
+++ src/leetcode/CountSortedVowelStrings.java
@@ -6,55 +6,38 @@ import java.util.*;
  * HashSet 은 순서 보장 하지 안는다. queue 에서 원소 뽑을 때마다, String 을 새로 만들어줘야 한다.
  */
 public class CountSortedVowelStrings {
-    public int countVowelStrings(int n) {
-        String[] vowels = {"a", "e", "i", "o", "u"};
-        Map<String, Integer> vowelOrder = new HashMap<>();
-        //assign order into each character
-        for (int i = 0; i < vowels.length; i++) {
-            vowelOrder.put(vowels[i], i);
-        }
-        Queue<String> queue = new LinkedList<>();
-        List<String> answer = new ArrayList<>();
 
-        for (String vowel : vowels) {
-            queue.offer(vowel);
+    Map<String, Integer> order = new HashMap<>(); //현재 키의 순서
+    int answer = 0;
+    String[] vowels = {"a","e","i","o","u"};
+    int[][] memo;
+
+    public int countVowelStrings(int n) {
+        memo = new int[n+1][5]; // cases. length - startIndex (현재 선택 가능한 모음의 시작 인덱스)
+        for (int[] row: memo) Arrays.fill(row, -1);
+        return dfs(n, 0);
+    }
+    //if there's no memoization, dfs until it returns and count the sum
+    public int dfs(int remainingLength, int startIndex) {
+        if (remainingLength == 0) return 1;
+
+        if (memo[remainingLength][startIndex]!=-1) {
+            return memo[remainingLength][startIndex];
         }
-        //StringBuilder 장점
-        if (n == 1) {
-            return 5;
+
+        int count = 0;
+
+        for (int i=startIndex;i<5;i++) {
+            count += dfs(remainingLength-1, i);
         }
-        while (!queue.isEmpty()) {
-            String temp = queue.poll();
-            String key = String.valueOf(temp.charAt(temp.length() - 1)); //  String -> char -> String
-            int lastLetterOrder = vowelOrder.get(key);
-            for (int i = 0; i < vowels.length; i++) {
-                if (lastLetterOrder <= i) {
-                    String newSt = temp + vowels[i];
-                    if (newSt.length() == n) {
-                        answer.add(newSt);
-                    } else {
-                        queue.offer(newSt);
-                    }
-                }
-            }
-        }
-        return answer.size();
+        memo[remainingLength][startIndex] = count;
+        return count;
     }
 
-//    public int countVowelStrings(int n) {
-//        Map<String, Boolean> visited = new HashMap<>();
-//        String[] vowels = {"a", "e", "i", "o", "u"};
-//        Map<String, Integer> vowelOrder = new HashMap<>();
-//        //assign order into each character
-//        for (int i = 0; i < vowels.length; i++) {
-//            vowelOrder.put(vowels[i], i);
-//        }
-//        Set<String> answer = new HashSet<>();
-//
-//    }
+
     public static void main(String[] args) {
         CountSortedVowelStrings st = new CountSortedVowelStrings();
-        System.out.println(st.countVowelStrings(1));
+        System.out.println(st.countVowelStrings(33));
     }
 }
 
