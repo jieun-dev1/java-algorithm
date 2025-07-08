e06e712cb42c8f1238e266dd09bcf3f895d37f0d
jieun <jkdev1324@gmail.com>
2025-07-08 21:03:30 +0900

    BinarySearch

---
+++ src/leetcode/BinarySearch.java
@@ -0,0 +1,53 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinarySearch {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return root;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode current = queue.poll();
+            if (current.val == val) {
+                return current;
+            }
+            if (current.left != null) {
+                queue.offer(current.left);
+            }
+            if (current.right != null) {
+                queue.offer(current.right);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BinarySearch bs = new BinarySearch();
+        // 트리 구성
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        System.out.println(bs.searchBST(root, 2));
+    }
+}
