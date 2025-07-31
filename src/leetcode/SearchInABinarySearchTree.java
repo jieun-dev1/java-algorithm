a556e1952f1cb0edc933ebbb00129f18faf8b5b5
jieun <jkdev1324@gmail.com>
2025-07-31 22:23:46 +0900

    Search

---
+++ src/leetcode/SearchInABinarySearchTree.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        while (root!= null) {
+            if (root.val == val) {
+                return root;
+            }
+
+            if(root.val > val) {
+                return searchBST(root.left, val);
+            }
+            if (root.val < val) {
+                return searchBST(root.right, val);
+            }
+        }
+
+        return null;
+    }
+}
