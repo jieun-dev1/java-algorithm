4b26ed19f39c26579140e562a391c06c4a7f1275
jieun <jkdev1324@gmail.com>
2025-09-27 11:09:44 +0900

    PriorityQueue - not initializing smallest infinite set

--- src/leetcode/PriorityQueue.java
+++ src/leetcode/PriorityQueue.java
@@ -1,30 +1,38 @@
 package leetcode;
 
-import java.util.HashSet;
-import java.util.PriorityQueue;
-import java.util.Set;
+import java.util.*;
 
 class SmallestInfiniteSet {
-
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
-    Set<Integer> removed = new HashSet<>();
+    private int smallest = 1;  // 현재 가장 작은 연속된 수
+    private PriorityQueue<Integer> pq = new PriorityQueue<>();
+    private Set<Integer> set = new HashSet<>();
 
     public SmallestInfiniteSet() {
-        for (int i=1; i<=1000; i++) {
-            pq.add(i);
-        }
+        // 무한집합은 개념적으로 존재, smallest로 추적
     }
 
     public int popSmallest() {
-        int element = pq.peek();
-        removed.add(element);
-        return pq.poll();
+        //if it exists in pq, it means it was added back. hence, should remove from set.
+        if (!pq.isEmpty()) {
+            int result = pq.poll();
+            set.remove(result);
+            return result;
+        }
+        return smallest++; //return and increase
     }
 
+    //addBack: number does not exist in a queue, and addedBack manually.
+
     public void addBack(int num) {
-        if (removed.contains(num)) {
-            removed.remove(num);
-            pq.add(num);
+        if (num < smallest && !set.contains(num)) {
+            pq.offer(num);
+            set.add(num);
         }
+        // wrong approach - I think I got confused when I initiated the range of nums and delete them.
+
+        // if (num < smallest && set.contains(num)) {
+        //     addedBackPq.offer(num);
+        //     set.remove(num);
+        // }
     }
 }
