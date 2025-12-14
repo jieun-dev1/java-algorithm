391eeb5399b8e459684bfe28fd252f6388c47f14
jieun <jkdev1324@gmail.com>
2025-12-14 20:37:42 +0900

    DFS

--- src/leetcode/CircleNum.java
+++ src/leetcode/CircleNum.java
@@ -1,4 +1,24 @@
 package leetcode;
 
 public class CircleNum {
+    public int findCircleNum(int[][] isConnected) {
+        boolean[] isVisited = new boolean[isConnected.length];
+        int count = 0;
+        for (int i=0; i<isConnected.length;i++) {
+            if (!isVisited[i]) {
+                dfs(isConnected, isVisited, i);
+                count ++;   //when not connected to anyone, still counted
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] isVisited, int from) {
+        isVisited[from] = true;
+        for (int i= 0; i<isConnected[from].length;i++) {
+            if (isConnected[from][i] == 1 && isVisited[i] == false) {
+                dfs(isConnected, isVisited, i);
+            }
+        }
+    }
 }
