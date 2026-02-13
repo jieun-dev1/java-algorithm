617dc8038cc726fd235aa0a89482f4db655cc179
jieun <jkdev1324@gmail.com>
2026-02-13 23:21:48 +0900

    fix one value and do two pointers O(N^2) but it ony beats 10%

---
+++ src/review/ThreeSum.java
@@ -0,0 +1,30 @@
+package review;
+
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        //Set<List<Integer>> 에 정렬해서 넣는다.
+        Set<List<Integer>> answers = new HashSet<>();
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i=0; i<nums.length -2; i++) {
+            int left = i+1; //i가 포함되니까, 그 오른쪽 부터
+            int right = nums.length - 1; //맨 끝까지.
+
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    answers.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left ++; //이미 left, right 조합은 나왔으니 가능한 조합은 무조건 left/right가 아니어야 함.
+                    right --;
+                } else if (sum < 0) {
+                    left ++;
+                } else {
+                    right --;
+                }
+            }
+        }
+        return new ArrayList<>(answers);
+    }
+}
