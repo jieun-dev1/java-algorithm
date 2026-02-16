0fe0e613085b09c5ff3155ab07c38ae34061716d
jieun <jkdev1324@gmail.com>
2026-02-16 16:42:45 +0900

    - marking with unique ids matter. Unless, we cannot differentiate islands (only unique ones should be summed so that there's no dups)

--- src/leetcode/graph/LargeIsland.java
+++ src/leetcode/graph/LargeIsland.java
@@ -3,89 +3,75 @@ package leetcode.graph;
 import java.util.*;
 
 class LargeIsland {
+
     int[] dx = {-1, 0, 1, 0};
     int[] dy = {0, -1, 0, 1};
-    List<int[]> locationOfZeros = new ArrayList<>();
 
-
-    public int largestIsland(int[][] grid){
+    public int largestIsland(int[][] grid) {
         //island size by index
         Map<Integer, Integer> islandSize = new HashMap<>();
-        Queue<int[]> queue = new LinkedList<>();
 
         int index = 2;
+        int maxResult = 0;
+        //n*n
+        int n = grid.length;
 
-        for (int i=0; i<grid.length; i++) {
-            for (int j=0; j<grid[0].length; j++) {
-                if (grid[i][j] == 0) {
-                    locationOfZeros.add(new int[]{i, j});
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    grid[i][j] = index;
-                    queue.add(new int[]{i, j});
-                    islandSize.put(index, bfs(queue, index++, grid));
+                    //do the dfs to find the size of island
+                    int result = find(i, j, index, grid);
+                    //mark the island with unique index
+                    islandSize.put(index, result);
+                    index ++;
+                    //in case there's no 0
+                    maxResult = Math.max(maxResult, result);
                 }
             }
         }
 
-        int maxSize = 0;
-
-        for (int i=0; i<locationOfZeros.size(); i++) {
-            int[] element = locationOfZeros.get(i);
-            Set<Integer> ids = new HashSet<>();
-            for (int j=0; j<4; j++) {
-                int nx = element[0] + dx[j];
-                int ny = element[1] + dy[j];
-
-                if (nx < 0 || nx > grid.length - 1 || ny < 0 || ny > grid[0].length - 1) {
-                    continue;
-                }
-
-                if (grid[nx][ny]!= 0) {
-                    ids.add(grid[nx][ny]);
+        //0의 위치 찾아서 그 가운데에 있는 0 flip 시 4방향 중 unique 한 애들만 더하기.
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 0) {
+                    Set<Integer> visited = new HashSet<>();
+                    int connectedSize = 1;
+                    for (int k=0; k<4; k++) {
+                        int nx = i + dx[k];
+                        int ny = j + dy[k];
+                        //방문한 적 있다면 넘긴다.
+                        if ( nx < 0 || nx > n - 1 || ny < 0 || ny > n - 1 || grid[nx][ny] == 0 || visited.contains(grid[nx][ny])) { continue; }
+                        connectedSize += islandSize.get(grid[nx][ny]);
+                        //make sure to check as visited
+                        visited.add(grid[nx][ny]);
+                    }
+                    maxResult = Math.max(connectedSize, maxResult);
                 }
             }
-            int currentSize = 0;
-            for(int id : ids) {
-                currentSize += islandSize.get(id);
-            }
-            //including the changing cell itself.
-            maxSize = Math.max(currentSize+1, maxSize);
+        }
+        return maxResult;
+    }
+    //get the size of island
+    public int find(int x, int y, int index, int[][] grid) {
+        int n = grid.length;
+
+        if (x < 0 || x > n - 1 || y < 0 || y > n  - 1) {
+            return 0;
+        }
+        if (grid[x][y] != 1) {
+            return 0;
         }
 
-        if (locationOfZeros.isEmpty()) {
-            maxSize = grid.length * grid[0].length;
-        }
-        return maxSize;
+        int cnt = 1;
+        //must be here before visiting 4 dir. unless one of four dir will visit the one calls it
+        grid[x][y] = index;
+
+        cnt += find(x + dx[0], y+ dy[0], index,  grid);
+        cnt += find(x + dx[1], y+ dy[1], index,  grid);
+        cnt += find(x + dx[2], y+ dy[2], index,  grid);
+        cnt += find(x + dx[3], y+ dy[3], index,  grid);
+
+        return cnt;
     }
 
-    public int bfs(Queue<int[]> queue, int id, int[][] grid) {
-        int size = 1;
-        while (!queue.isEmpty()) {
-            int[] element = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = element[0] + dx[i];
-                int ny = element[1] + dy[i];
-                if (nx < 0 || nx > grid.length-1 || ny < 0 || ny > grid[0].length -1) {
-                    continue;
-                }
-                if (grid[nx][ny] == 0) {
-                    locationOfZeros.add(new int[]{nx, ny});
-                    continue;
-                }
-                if (grid[nx][ny] == 1) {
-                    grid[nx][ny] = id;
-                    queue.offer(new int[]{nx, ny});
-                    size++;
-                }
-            }
-        }
-        return size;
-    }
-
-    public static void main(String[] args) {
-        LargeIsland largeIsland = new LargeIsland();
-        int[][] grid = new int[][]{{1,1},{1,1}};
-        System.out.println(largeIsland.largestIsland(grid));
-    }
 }
